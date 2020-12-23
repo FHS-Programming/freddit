fc93b8645b060ffad7706a3d6b24ce2889be6ec0
Unknown dragon <sanskarpok11@gmail.com>
2020-12-22 22:21:46 -0500

    sibe bar closes with animation

--- src/pages/sidebar/Sidebar.js
+++ src/pages/sidebar/Sidebar.js
@@ -1,14 +1,22 @@
-import React from "react";
+import React from 'react';
 import "./Sidebar.css";
 import Overlay from "./Overlay";
 
 
 export default function Sidebar(props) {
+   const Bye =  () => {
+    var hello = document.getElementById('sideBar');
+     hello.classList.add('closeSB')
+    setTimeout(() => {
+     return props.toggle();
+  }, 100);
+  }
+
   return (
     <>
-      <Overlay onClick={props.toggle} />
+      <Overlay onClick={Bye} />
       <nav className="sideBar" id="sideBar">
-        <div className="close" onClick={props.toggle}>
+        <div className="close" onClick={Bye}>
           X
         </div>
         <ul>
